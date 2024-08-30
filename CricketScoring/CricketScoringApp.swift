fe50742532c7979b85ac29b6395490eacd7bc42d
Vishnu Kadiyala <vishnupk@ou.edu>
2024-08-30 15:00:26 -0500

    Initial Commit

---
+++ CricketScoring/CricketScoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricketScoringApp.swift
+//  CricketScoring
+//
+//  Created by Vishnu Kadiyala on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CricketScoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
